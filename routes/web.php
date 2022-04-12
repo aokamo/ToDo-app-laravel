3e34b3c103579ff0a797542cc9a3fbe7e417051a
atsushi-okamoto <a.okamoto20@gmail.com>
2022-04-12 22:29:39 +0900

    TaskControllerテスト

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,4 @@
 use Illuminate\Support\Facades\Route;
 
 // フォルダ一覧ページを表示
-Route::get('/folders/{id}/tasks'. 'TaskController@index')->name('tasks.index');
+Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
